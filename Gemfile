b28f53381da316f17adc52967c59e914ece84d06
Daniel Samuel <dansam5000@gmail.com>
2023-11-20 11:59:09 +0100

    Add dep for backend integration

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rubocop'
+gem 'sinatra'
+gem 'httparty'
+gem "redis"
+gem 'json'
--- app.rb
+++ app.rb
@@ -1,1 +1,15 @@
 #!/usr/bin/env ruby
+require 'sinatra'
+require 'json'
+require_relative 'lib/movies_client'
+
+get '/' do
+  send_file File.join(settings.public_folder, 'index.html')
+end
+
+post '/search' do
+  content_type :json
+  movie_name = params[:movie_name]
+
+  {results: "Hello #{movie_name}"}
+end
